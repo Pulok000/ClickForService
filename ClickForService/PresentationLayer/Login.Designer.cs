3fac0dc3453499ea2d5b503d124cdb12b726db36
jnjohnny <19-39366-1@student.aiub.edu>
2021-04-27 17:27:11 +0600

    Login Edit & Registradion Form Create

--- ClickForService/PresentationLayer/Login.Designer.cs
+++ ClickForService/PresentationLayer/Login.Designer.cs
@@ -29,12 +29,89 @@ namespace ClickForService.PresentationLayer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.Usernamelabel = new System.Windows.Forms.Label();
+            this.passwordlabel = new System.Windows.Forms.Label();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(325, 276);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(96, 34);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(351, 147);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(173, 20);
+            this.textBoxUsername.TabIndex = 1;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(351, 196);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(173, 20);
+            this.textBoxPassword.TabIndex = 2;
+            // 
+            // Usernamelabel
+            // 
+            this.Usernamelabel.AutoSize = true;
+            this.Usernamelabel.Location = new System.Drawing.Point(254, 150);
+            this.Usernamelabel.Name = "Usernamelabel";
+            this.Usernamelabel.Size = new System.Drawing.Size(60, 13);
+            this.Usernamelabel.TabIndex = 3;
+            this.Usernamelabel.Text = "User Name";
+            // 
+            // passwordlabel
+            // 
+            this.passwordlabel.AutoSize = true;
+            this.passwordlabel.Location = new System.Drawing.Point(254, 203);
+            this.passwordlabel.Name = "passwordlabel";
+            this.passwordlabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordlabel.TabIndex = 4;
+            this.passwordlabel.Text = "Password";
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(470, 276);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(98, 34);
+            this.buttonRegister.TabIndex = 5;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.passwordlabel);
+            this.Controls.Add(this.Usernamelabel);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.LoginButton);
+            this.Name = "Login";
             this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label Usernamelabel;
+        private System.Windows.Forms.Label passwordlabel;
+        private System.Windows.Forms.Button buttonRegister;
     }
 }
