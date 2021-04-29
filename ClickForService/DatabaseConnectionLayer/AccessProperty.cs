c19a0437473162c71e78ebc78aa91136522ca80a
jnjohnny <19-39366-1@student.aiub.edu>
2021-04-29 21:57:02 +0600

    Public Property Create

---
+++ ClickForService/DatabaseConnectionLayer/AccessProperty.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickForService.DatabaseConnectionLayer
+{
+    class AccessProperty
+    {
+        public string EmailId
+        {
+            set;
+            get;
+        }
+
+        public string Password
+        {
+            set;
+            get;
+        }
+
+        public string Category
+        {
+            set;
+            get;
+        }
+
+        public string Address 
+        {
+            set;
+            get;
+        }
+
+        public string City
+        {
+            set;
+            get;
+        }
+
+        public string Division
+        {
+            set;
+            get;
+        }
+
+        public string BloodGroup
+        {
+            set;
+            get;
+        }
+
+        public string MobileNumber
+        {
+            set;
+            get;
+        }
+
+        public string NID
+        {
+            set;
+            get;
+        }
+
+        public string FullName
+        {
+            set;
+            get;
+        }
+
+        public string DOB
+        {
+            set;
+            get;
+        }
+
+        public string Gender
+        {
+            set;
+            get;
+        }
+    }
+}
