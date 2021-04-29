4acbe43b51378e840ec5c736e4a6521120647ef5
jnjohnny <19-39366-1@student.aiub.edu>
2021-04-29 15:40:23 +0600

    Updates in

---
+++ ClickForService/BusinessLogicLayer/GeneralOperations.cs
@@ -0,0 +1,14 @@
+﻿using ClickForService.DatabaseConnectionLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickForService.BusinessLogicLayer
+{
+    class GeneralOperations
+    {
+        Operations Operation; 
+    }
+}
