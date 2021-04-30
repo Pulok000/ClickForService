4962a35cb5a98f37a04e0ed506fddf9c23277e1d
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2021-04-30 11:16:00 +0600

    Registration Form Validity

--- ClickForService/PresentationLayer/Registration.cs
+++ ClickForService/PresentationLayer/Registration.cs
@@ -101,44 +101,96 @@ namespace ClickForService.PresentationLayer
             //this.Hide();
             //login.Show();
 
-            SqlConnection connect = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString);
-            connect.Open();
-            string Gender = "";
-            if (MaleregisterradioButton.Checked)
+            if (AgreeregistercheckBox.Text == "")
             {
-                Gender = "Male";
+                MessageBox.Show("agree can not be empty");
             }
+
+            else if (NameregistertextBox.Text == "")
+            { MessageBox.Show("Name can not be empty"); }
+
+            else if (UsernameregistertextBox.Text == "")
+            { MessageBox.Show("User Name can not be empty"); }
+
+            else if (PasswordregistertextBox.Text == "")
+            { MessageBox.Show("Password can not be empty"); }
+
+            else if (ConfirmpasswordregistertextBox.Text == "")
+            { MessageBox.Show("Confrim Password can not be empty"); }
+
+            else if (EmailregistertextBox.Text == "")
+            { MessageBox.Show("Email can not be empty"); }
+
+            else if (DateofbirthregisterdateTimePicker.Text == "")
+            { MessageBox.Show("Date of Birth can not be empty"); }
+
+            else if (MaleregisterradioButton.Text == "" && FemaleregisterradioButton.Text == "")
+            { MessageBox.Show("Gender can not be empty"); }
+
+            else if (CityregistercomboBox.Text == "")
+            { MessageBox.Show("Blood Group can not be empty"); }
+
+            else if (NidregistertextBox.Text == "")
+            { MessageBox.Show("Blood Group can not be empty"); }
+
+            else if (MobilenumberregistertextBox.Text == "")
+            { MessageBox.Show("Blood Group can not be empty"); }
+
+            else if (DivisionregistercomboBox.Text == "")
+            { MessageBox.Show("Blood Group can not be empty"); }
+
+            else if (AddressregistertextBox.Text == "")
+            { MessageBox.Show("Blood Group can not be empty"); }
+
+            else if (CategoryregistercomboBox.Text == "")
+            { MessageBox.Show("Blood Group can not be empty"); }
+
+
+
+
+
             else
             {
-                Gender = "Female";
-            }
+                SqlConnection connect = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString);
+                connect.Open();
+                string Gender = "";
+                if (MaleregisterradioButton.Checked)
+                {
+                    Gender = "Male";
+                }
+                else
+                {
+                    Gender = "Female";
+                }
 
-            string sql1 = "INSERT INTO Registrations(userName, emailId, password, category, Address, City, Division, bloodGroup, mobileNumber, NID, fullName, dateofbirth, gender, Profession) VALUES('" + UsernameregistertextBox.Text + "','"+EmailregistertextBox.Text+ "', '" + PasswordregistertextBox.Text + "','" + CategoryregistercomboBox.Text + "','" + AddressregistertextBox.Text + "','" + CityregistercomboBox.Text + "','" + DivisionregistercomboBox.Text + "','" + BloodgroupregistercomboBox.Text + "','" + MobilenumberregistertextBox.Text + "','" + NidregistertextBox.Text + "','" + NameregistertextBox.Text + "','" + DateofbirthregisterdateTimePicker.Text + "','" + Gender + "','" + ProfessionregistercomboBox.Text + "')";
+                string sql1 = "INSERT INTO Registrations(userName, emailId, password, category, Address, City, Division, bloodGroup, mobileNumber, NID, fullName, dateofbirth, gender, Profession) VALUES('" + UsernameregistertextBox.Text + "','" + EmailregistertextBox.Text + "', '" + PasswordregistertextBox.Text + "','" + CategoryregistercomboBox.Text + "','" + AddressregistertextBox.Text + "','" + CityregistercomboBox.Text + "','" + DivisionregistercomboBox.Text + "','" + BloodgroupregistercomboBox.Text + "','" + MobilenumberregistertextBox.Text + "','" + NidregistertextBox.Text + "','" + NameregistertextBox.Text + "','" + DateofbirthregisterdateTimePicker.Text + "','" + Gender + "','" + ProfessionregistercomboBox.Text + "')";
 
-            SqlCommand command1 = new SqlCommand(sql1, connect);
-            int result = command1.ExecuteNonQuery();
-            if (result > 0)
-            {
-                MessageBox.Show("Registration Sucessful....Please Login");
-                
+                SqlCommand command1 = new SqlCommand(sql1, connect);
+                int result = command1.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    MessageBox.Show("Registration Sucessful....Please Login");
+
+                    connect.Close();
+
+
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                    connect.Close();
+
+                }
+
+
+
+
+
                 connect.Close();
-
-
-
-
-
             }
-            else
-            {
-                MessageBox.Show("Error");
-                connect.Close();
-
-            }
-
-
-
-
-            connect.Close();
 
 
 
