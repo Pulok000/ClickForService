b93865f0f83094b9d031b1e4fcff4bea83a0a305
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2021-04-30 23:46:25 +0600

    admin

--- ClickForService/PresentationLayer/DashboardforAdmin.Designer.cs
+++ ClickForService/PresentationLayer/DashboardforAdmin.Designer.cs
@@ -38,97 +38,151 @@ namespace ClickForService.PresentationLayer
             this.buttonBU = new System.Windows.Forms.Button();
             this.buttonMP = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.label16 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonUserList
             // 
-            this.buttonUserList.Location = new System.Drawing.Point(74, 58);
+            this.buttonUserList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonUserList.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonUserList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUserList.Location = new System.Drawing.Point(72, 125);
             this.buttonUserList.Name = "buttonUserList";
             this.buttonUserList.Size = new System.Drawing.Size(107, 70);
             this.buttonUserList.TabIndex = 0;
             this.buttonUserList.Text = "User List";
-            this.buttonUserList.UseVisualStyleBackColor = true;
+            this.buttonUserList.UseVisualStyleBackColor = false;
             // 
             // buttonUC
             // 
-            this.buttonUC.Location = new System.Drawing.Point(417, 58);
+            this.buttonUC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonUC.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonUC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUC.Location = new System.Drawing.Point(415, 125);
             this.buttonUC.Name = "buttonUC";
             this.buttonUC.Size = new System.Drawing.Size(107, 70);
             this.buttonUC.TabIndex = 1;
             this.buttonUC.Text = "User Complain";
-            this.buttonUC.UseVisualStyleBackColor = true;
+            this.buttonUC.UseVisualStyleBackColor = false;
             this.buttonUC.Click += new System.EventHandler(this.button2_Click);
             // 
             // buttonSPL
             // 
-            this.buttonSPL.Location = new System.Drawing.Point(254, 58);
+            this.buttonSPL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonSPL.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSPL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSPL.Location = new System.Drawing.Point(252, 125);
             this.buttonSPL.Name = "buttonSPL";
             this.buttonSPL.Size = new System.Drawing.Size(107, 70);
             this.buttonSPL.TabIndex = 2;
             this.buttonSPL.Text = "Service Provider List";
-            this.buttonSPL.UseVisualStyleBackColor = true;
+            this.buttonSPL.UseVisualStyleBackColor = false;
             this.buttonSPL.Click += new System.EventHandler(this.button3_Click);
             // 
             // buttonUPU
             // 
-            this.buttonUPU.Location = new System.Drawing.Point(572, 58);
+            this.buttonUPU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonUPU.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonUPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUPU.Location = new System.Drawing.Point(570, 125);
             this.buttonUPU.Name = "buttonUPU";
-            this.buttonUPU.Size = new System.Drawing.Size(107, 70);
+            this.buttonUPU.Size = new System.Drawing.Size(115, 70);
             this.buttonUPU.TabIndex = 3;
             this.buttonUPU.Text = "Users Profile Edit/Update";
-            this.buttonUPU.UseVisualStyleBackColor = true;
+            this.buttonUPU.UseVisualStyleBackColor = false;
             // 
             // buttonSI
             // 
-            this.buttonSI.Location = new System.Drawing.Point(74, 187);
+            this.buttonSI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonSI.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSI.Location = new System.Drawing.Point(61, 254);
             this.buttonSI.Name = "buttonSI";
-            this.buttonSI.Size = new System.Drawing.Size(107, 70);
+            this.buttonSI.Size = new System.Drawing.Size(118, 70);
             this.buttonSI.TabIndex = 4;
             this.buttonSI.Text = "Subscription Issue";
-            this.buttonSI.UseVisualStyleBackColor = true;
+            this.buttonSI.UseVisualStyleBackColor = false;
             // 
             // buttonAL
             // 
-            this.buttonAL.Location = new System.Drawing.Point(254, 187);
+            this.buttonAL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonAL.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAL.Location = new System.Drawing.Point(252, 254);
             this.buttonAL.Name = "buttonAL";
             this.buttonAL.Size = new System.Drawing.Size(107, 70);
             this.buttonAL.TabIndex = 5;
             this.buttonAL.Text = "Admin\'s List";
-            this.buttonAL.UseVisualStyleBackColor = true;
+            this.buttonAL.UseVisualStyleBackColor = false;
             // 
             // buttonBU
             // 
-            this.buttonBU.Location = new System.Drawing.Point(417, 187);
+            this.buttonBU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonBU.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonBU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBU.Location = new System.Drawing.Point(415, 254);
             this.buttonBU.Name = "buttonBU";
             this.buttonBU.Size = new System.Drawing.Size(107, 70);
             this.buttonBU.TabIndex = 6;
             this.buttonBU.Text = "Block User";
-            this.buttonBU.UseVisualStyleBackColor = true;
+            this.buttonBU.UseVisualStyleBackColor = false;
             // 
             // buttonMP
             // 
-            this.buttonMP.Location = new System.Drawing.Point(572, 187);
+            this.buttonMP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonMP.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonMP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMP.Location = new System.Drawing.Point(570, 254);
             this.buttonMP.Name = "buttonMP";
             this.buttonMP.Size = new System.Drawing.Size(107, 70);
             this.buttonMP.TabIndex = 7;
             this.buttonMP.Text = "My Profile";
-            this.buttonMP.UseVisualStyleBackColor = true;
+            this.buttonMP.UseVisualStyleBackColor = false;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(685, 345);
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(679, 384);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(76, 27);
+            this.button9.Size = new System.Drawing.Size(86, 30);
             this.button9.TabIndex = 8;
             this.button9.Text = "Log Out";
-            this.button9.UseVisualStyleBackColor = true;
+            this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label16.Location = new System.Drawing.Point(291, 83);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(205, 28);
+            this.label16.TabIndex = 67;
+            this.label16.Text = "Click For Service";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ClickForService.Properties.Resources.clickservicelogo;
+            this.pictureBox1.Location = new System.Drawing.Point(343, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(87, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 66;
+            this.pictureBox1.TabStop = false;
             // 
             // DashboardforAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.buttonMP);
             this.Controls.Add(this.buttonBU);
@@ -141,7 +195,9 @@ namespace ClickForService.PresentationLayer
             this.Name = "DashboardforAdmin";
             this.Text = "DashboardforAdmin";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DashboardforAdmin_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -156,5 +212,7 @@ namespace ClickForService.PresentationLayer
         private System.Windows.Forms.Button buttonBU;
         private System.Windows.Forms.Button buttonMP;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
