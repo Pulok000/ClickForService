7c50349b09e7d52e8381cf292630843eacb19c0d
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2021-05-01 10:23:52 +0600

    new form mawa

---
+++ ClickForService/PresentationLayer/CarServicing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickForService.PresentationLayer
+{
+    public partial class CarServicing : Form
+    {
+        public CarServicing()
+        {
+            InitializeComponent();
+        }
+
+        private void CarServicing_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
