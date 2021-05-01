3006741411ed305df2a89a4899fe8a18be872618
jnjohnny <19-39366-1@student.aiub.edu>
2021-05-01 10:15:39 +0600

    Admin My profile Data Access

--- ClickForService/PresentationLayer/AdminMyProfile.cs
+++ ClickForService/PresentationLayer/AdminMyProfile.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ClickForService.DatabaseConnectionLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +22,32 @@ namespace ClickForService.PresentationLayer
         private void AdminMyProfile_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdminMyProfile_Load(object sender, EventArgs e)
+        {
+            DataAcess da = new DataAcess();
+
+            string sql = "SELECT *FROM Registrations WHERE userName= '" + Login.UserName + "'";
+
+            da.GetData(sql);
+            SqlDataReader reader = da.GetData(sql);
+
+            if (reader.Read())
+            {
+                 Nametextbox.Text = Convert.ToString(reader["fullName"]);
+                 UNtextbox.Text = Convert.ToString(reader["emailId"]);
+                //textBoxPass.Text = Convert.ToString(reader["password"]);
+                //Textboxgender.Text = Convert.ToString(reader["gender"]);
+                da.ConnectionClose();
+
+
+            }
         }
     }
 }
