00d77e9477247f159fd4e1ded398e3507f6d19cd
jnjohnny <19-39366-1@student.aiub.edu>
2021-05-01 22:58:05 +0600

    UserProfile Edit From Admin

---
+++ ClickForService/PresentationLayer/UserProfileInAdmin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClickForService.PresentationLayer
+{
+    public partial class UserProfileInAdmin : Form
+    {
+        public UserProfileInAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
