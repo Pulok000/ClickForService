c217cc6c0caf8a571bf369ed3ddbd9dfcf3e04fb
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2021-05-01 10:40:20 +0600

    car servicing form all done mawa

--- ClickForService/PresentationLayer/CarServicing.cs
+++ ClickForService/PresentationLayer/CarServicing.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using ClickForService.DatabaseConnectionLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,5 +24,65 @@ namespace ClickForService.PresentationLayer
         {
             Application.Exit();
         }
+
+        private void CarServicing_Load(object sender, EventArgs e)
+        {
+            string sql = "SELECT *FROM Registrations WHERE userName='" + Login.UserName + "'";
+            AccessProperty ap = new AccessProperty();
+            DataAcess da = new DataAcess();
+            SqlDataReader reader = da.GetData(sql);
+            while (reader.Read())
+            {
+                textBoxCity.Text = Convert.ToString(reader["City"]);
+
+            }
+            da.ConnectionClose();
+        }
+
+        int pos;
+        SqlDataAdapter sqladp;
+        DataTable data = new DataTable();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString);
+
+            DataAcess ac = new DataAcess();
+            string PF = "Car Mechanic";
+            string sql = "SELECT *FROM Registrations WHERE City='" + textBoxCity.Text + "'AND gender='" + comboBox1.Text + "'AND Profession='" + PF + "'";
+            sqladp = new SqlDataAdapter(sql, connection);
+            sqladp.Fill(data);
+            pos = 0;
+            displaytext(pos);
+
+        }
+        public void displaytext(int rowno)
+        {
+            textBox2.Text = data.Rows[rowno][10].ToString();
+            textBox3.Text = data.Rows[rowno][4].ToString();
+            textBox4.Text = data.Rows[rowno][8].ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pos = pos - 1;
+            if (pos <= data.Rows.Count)
+            {
+                pos++;
+                MessageBox.Show("First Record");
+            }
+            displaytext(pos);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pos = pos + 1;
+            if (pos >= data.Rows.Count)
+            {
+                pos--;
+                MessageBox.Show("Last Record");
+            }
+            displaytext(pos);
+        }
     }
 }
+
