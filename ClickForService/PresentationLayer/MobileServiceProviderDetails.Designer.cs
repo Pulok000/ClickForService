e9153819b2ecae5587e244280ca171c1b112a22d
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2021-05-02 10:10:27 +0600

    mbspd2

--- ClickForService/PresentationLayer/MobileServiceProviderDetails.Designer.cs
+++ ClickForService/PresentationLayer/MobileServiceProviderDetails.Designer.cs
@@ -340,7 +340,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(583, 526);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.availabilitytextBox);
