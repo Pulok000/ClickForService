2680ecc5089cb9d4cf2e5ae956504e85ec27da2f
19-39353-1@student.aiub.edu <19-39353-1@student.aiub.edu>
2021-05-02 10:49:44 +0600

    update

--- ClickForService/PresentationLayer/AddServiceProviderDetails.Designer.cs
+++ ClickForService/PresentationLayer/AddServiceProviderDetails.Designer.cs
@@ -41,7 +41,6 @@
             this.NameregistertextBox = new System.Windows.Forms.TextBox();
             this.label16 = new System.Windows.Forms.Label();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             this.SuspendLayout();
             // 
@@ -202,24 +201,12 @@
             this.pictureBox7.TabIndex = 140;
             this.pictureBox7.TabStop = false;
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 456);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 33);
-            this.button1.TabIndex = 142;
-            this.button1.Text = "Back";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
             // AddServiceProviderDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(555, 508);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label16);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.totimecomboBox);
@@ -258,6 +245,5 @@
         private System.Windows.Forms.TextBox NameregistertextBox;
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.PictureBox pictureBox7;
-        private System.Windows.Forms.Button button1;
     }
 }
