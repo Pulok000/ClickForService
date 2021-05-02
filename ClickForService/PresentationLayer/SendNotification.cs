0127c85d42ba98c82332781f67eb673d85cece4c
jnjohnny <19-39366-1@student.aiub.edu>
2021-05-02 09:50:13 +0600

    Notification to users

--- ClickForService/PresentationLayer/SendNotification.cs
+++ ClickForService/PresentationLayer/SendNotification.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -30,7 +32,34 @@ namespace ClickForService.PresentationLayer
             popup.ContentText = textBox1.Text;
             textBox1.Text = "";
 
-            popup.Popup();
+            
+
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString);
+
+            connection.Open();
+
+            string sql1 = "SELECT *FROM userpermissions WHERE (uniqueCode='" + "AD" + "' AND uniqueCode='" + "ST" + "' OR uniqueCode='" + "GU" + "' OR uniqueCode='" + "" + "')";
+            SqlCommand command = new SqlCommand(sql1, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+
+            textBox1.Text = "";
+
+            if (reader.Read())
+            {
+                popup.Popup();
+                connection.Close();
+
+            }
+            else
+            {
+
+                MessageBox.Show("Sorry! There is No User Found");
+                connection.Close();
+            }
+
+
+
         }
 
         private void SendNotification_FormClosing(object sender, FormClosingEventArgs e)
